158bdbdd3a5b0639ab2b4c1788b2d3a047f5de7c
TodescoAmandine <todescoamandine@yahoo.fr>
2023-11-27 15:23:01 +0100

    AddEventL sur arrows et début boucle select Dots

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 const slides = [
 	{
 		"image": "slide1.jpg",
@@ -16,20 +17,106 @@ const slides = [
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-//let arrow_right = document.getElementById("arrow_right")
-//console.log("arrow_right")
-//let listearrow = document.getElementById("arrow");
-//listearrow.addEventListener("click", () => {
 
-///let arrow_right = document.querySelector("arrow_right")
-///arrow_right.style.color = "red"
-//});
 
-const arrowR = document.querySelector('.arrow_right')
+
+/******* ALERTE SUR EVENT LISTENER FLECHES*/
+
+/*avec classe
+
+let arrowR = document.querySelector('.arrow_right')
 arrowR.addEventListener('click', function () {
 	alert('Bonjour')
 })
-const arrowL = document.querySelector('.arrow_left')
+let arrowL = document.querySelector('.arrow_left')
+arrowL.addEventListener('click', function () {
+	alert('Bonjour')
+})*/
+
+
+
+/*avec ID*/
+let arrowR = document.getElementById('arrow_right')
 arrowR.addEventListener('click', function () {
 	alert('Bonjour')
 })
+let arrowL = document.getElementById('arrow_left')
+arrowL.addEventListener('click', function () {
+	alert('Bonjour')
+})
+
+/****DIAPOSITIVE EN COURS DE VISIONNAGE*** */
+
+/*on créé une const et on récupère les inputs dots*/
+const dots = document.querySelectorAll(".dots input")
+
+/*on créé une fonction qui contient une boucle pour connaîte la place de l'élément selectionné
+on appel (n) nombre de l'endroit de l'object séléctionné...???
+const elem récupère la place des dots dans le "tableau"lenght = i
+On remove le dot selected dans la classe des élém 
+et on ajoute dot selected à la classe selectionné
+ */
+function selectDot(n) {
+	for (let i = 0; i < dots.length; i++) {
+		const elem = dots[i];
+		elem.classList.remove("dot_selected");
+		if (i == n) {
+			// ajouter la classe selected a l'element
+			elem.classList.add('dot_selected');
+		}
+	}
+}
+
+/*maintenant que l'on connait la place de dot selected grâce à la fonction, on créé une boucle pour appliquer un addlistener au click*/
+for (let i = 0; i < dots.length; i++) {
+	const elem = dots[i];
+	elem.addEventListener('click', () => {
+		selectDot(i)/*applique le addEventL au clic à selectDOt récupéré*/
+		console.log('dot_selected')
+
+	})
+
+}
+/*est ce que cela corresopnd à un compteur ??*/
+selectDot(0)
+
+
+
+
+
+/* 
+essai slide
+const items = document.querySelectorAll(".slider img");
+const nbSlide = items.length;
+const suivant = document.querySelector('.arrow_right');
+const precedent = document.querySelector('.arrow_left');
+let count = 0;
+
+function slideSuivante() {
+	items[count].classList.remove('active');
+
+	if (count < nbSlide - 1) {
+		count++;
+	} else {
+		count = 0;
+	}
+	items[count].classList.add('active')
+	console.log(items[count]);
+}
+suivant.addEventListener('click', slideSuivante)
+
+function slidePrecedente() {
+	items[count].classList.remove('active');
+
+	if (count > 0) {
+		count--;
+	} else {
+		count = nbSlide - 1;
+
+	}
+
+	items[count].classList.add('active')
+	console.log(items[count]);
+
+}
+precedent.addEventListener('click', slidePrecedente) */
